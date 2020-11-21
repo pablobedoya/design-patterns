4d70670b138f084aac99cb62914aa3d89e165da4
Pablo Bedoya <pablo.reds@hotmail.com>
2020-11-21 20:58:09 -0300

    Implementing facade pattern

---
+++ estruturais/facade/src/main/java/facade/RestaurantOrderFacade.java
@@ -0,0 +1,20 @@
+package facade;
+
+import facade.restaurant.Kitchen;
+import facade.restaurant.Waiter;
+
+public class RestaurantOrderFacade {
+    
+    private Waiter waiter = new Waiter();
+    private Kitchen kitchen = new Kitchen();
+
+    public void orderMeal() {
+        waiter.writeOrder();
+        waiter.sendMealToKitchen();
+        kitchen.selectIngredients();
+        kitchen.prepareMeal();
+        kitchen.callWaiter();
+        waiter.serveMeal();
+    }
+
+}
---
+++ estruturais/facade/src/main/java/facade/restaurant/Kitchen.java
@@ -0,0 +1,22 @@
+package facade.restaurant;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Kitchen {
+    
+    private static final Logger LOGGER = LogManager.getLogger(Kitchen.class);
+    
+    public void selectIngredients() {
+        LOGGER.info("Selecting the ingredients to prepare the meal...");
+    }
+    
+    public void prepareMeal() {
+        LOGGER.info("Preparing the meal.");
+    }
+
+    public void callWaiter() {
+        LOGGER.info("Calling the waiter to serve the meal to the customer.");
+    }
+
+}
